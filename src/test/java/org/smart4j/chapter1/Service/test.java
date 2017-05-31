7162b83410c7dec1c60fcd4e464c5a7e0af0520c
SingleLa <394950814@qq.com>
2017-05-31 18:37:23 +0800

    classLoader

--- src/test/java/org/smart4j/chapter1/Service/test.java
+++ src/test/java/org/smart4j/chapter1/Service/test.java
@@ -1,11 +1,14 @@
 package org.smart4j.chapter1.Service;
 
 import org.junit.Test;
+import org.smart4j.chapter1.Helper.ConfigHelper;
 import org.smart4j.chapter1.Model.Customer;
+import org.smart4j.chapter1.Util.ClassUtil;
 import org.smart4j.chapter1.Util.ProsUtil;
 
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * Created by Administrator on 2017/5/31.
@@ -18,7 +21,8 @@ public class test {
     }
     @Test
     public void test1(){
-       boolean b = customerService.createCustomer();
+        Set<Class<?>> classSet = ClassUtil.getClassSet(ConfigHelper.getAppBasePackage());
+        System.out.print(classSet);
 
     }
 }

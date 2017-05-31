b754cf3d5c813808bf6dff300f5fe0ec3810f3c9
SingleLa <394950814@qq.com>
2017-05-31 19:18:07 +0800

    注解类

---
+++ src/main/java/org/smart4j/chapter1/Annotation/Action.java
@@ -0,0 +1,18 @@
+package org.smart4j.chapter1.Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Administrator on 2017/5/31.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Action {
+    /**
+     * 请求类型与路径
+     */
+    String value();
+}
